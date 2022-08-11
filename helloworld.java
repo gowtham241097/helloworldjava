c5b78261a0faa10be838ba46d12e8036220d389c
gowtham241097 <110183786+gowtham241097@users.noreply.github.com>
2022-08-11 19:16:07 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World to all");
+    System.out.println("Hello World to all triggerd by the github webhook");
     }
 }
